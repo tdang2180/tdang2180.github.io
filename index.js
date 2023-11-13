6146bb2efb1b139b09a24048840605f41cb4cf08
tdang2180 <114024874+tdang2180@users.noreply.github.com>
2023-11-12 17:06:05 -0800

    use create root to render app.js onto document

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { reactDom } from "react-dom";
-import App from "./src/App"
+import ReactDOM from "react-dom/client";
+import App from "./src/App.js"
 
-reactDom.render(<App/>, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
