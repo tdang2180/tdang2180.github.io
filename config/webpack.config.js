aa56b255bb14cbb678e0f7338b9f967c1860d054
Thanh <114024874+tdang2180@users.noreply.github.com>
2023-11-16 12:53:21 -0800

    Development Server  (#2)
    * organized files into public and src folders
    
    * use create root to render app.js onto document
    
    * npm init and install webpack dependencies
    
    * moved index.js to src folder
    
    * added start and build scripts to package.json
    
    * first time configured webpack
    
    * first time configured babel
    
    * installed babel into devDep and react into dependencies
    
    * changed start script to use webpack config
    
    * changed js files to jsx
    
    * added babel/preset-react option to module rules

---
+++ config/webpack.config.js
@@ -0,0 +1,86 @@
+const path = require("path")
+
+module.exports = {
+  /** "mode"
+   * the environment - development, production, none. tells webpack
+   * to use its built-in optimizations accordingly. default is production
+   */
+  mode: "development",
+
+  /** "entry" the entry point
+   */
+  entry: "./src/index.jsx",
+
+  output: {
+    /** "path" the folder path of the output file
+     */
+    path: path.resolve(__dirname, "../public"),
+
+    /** "filename" the name of the output file
+     */
+    filename: "main.js",
+
+    /** "clean" clean the output directory before emit
+     */
+    clean: true,
+  },
+
+  /** "resolve"
+     * Attempt to resolve these extensions in order. If multiple files share the same 
+     * name but have different extensions, webpack will resolve the one with the extension 
+     * listed first in the array and skip the rest.
+     */
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+
+
+  /** "module" determine how the different types of modules will be treated
+   */
+  module: {
+    /** "rules" specify how webpack should transform, process, 
+     * or handle modules during the budling process
+     */
+    rules: [
+     {
+      /** "test" transform code when a file resolves to '.js or .jsx'
+       */
+      test: /\.(js|jsx)$/,
+
+      /** "exclude" exclude node_module folder from being searched
+       */
+      exclude: /node.modules/,
+
+      /** "use" use the babel-loader to transform it before you
+       */
+      use: {
+        loader: 'babel-loader',    
+        options: { 
+          presets: ['@babel/env', '@babel/preset-react'] 
+        },
+      }
+      } 
+    ]
+  },
+
+  /** "devServer" create a dev server using webpack-dev-server
+   */
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "../public"),
+    },
+    /** "port" port of dev server
+     */
+    port: 9000,
+
+    /** "compress"
+     * reduce the size of transmitted data, faster loading times
+     */
+    compress: true,
+
+    /** "open" open the browser when the server is successfully started
+     */
+    open: true,
+
+  },
+};
